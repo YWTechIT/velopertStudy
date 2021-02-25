4832ef20f0c41805292a74771de67546a69acd54
YWTechIT <abcd8637@naver.com>
2021-02-25 22:00:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@
 
 import "./App.css";
 import react, { Component, useState } from "react";
-import SassComponent from './SASS/SassComponent';
 
 const App = () => {
+  function increase(number) {
+    const newPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const result = number + 10;
+        if (result > 10) {
+          const error = new Error("에러낫다리");
+          return reject(error)
+        }
+        resolve(result);
+      }, 1000);
+    });
+    return newPromise;
+  }
+
+  increase(0)
+  .then(number => {
+    console.log(number);
+    return increase(number)
+  })
+  .then(number => {
+    console.log(number);
+    return increase(number)
+  })
+  .then(number => {
+    console.log(number);
+    return increase(number)
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
   return (
     <>
-      <SassComponent/>
+      <h1>안녕하세요!</h1>
     </>
   );
 };
